4ef40eb963d20130a4f2ea599e5091e227ba9af2
Ruslan <sichkaryk39@gmail.com>
2023-02-27 18:16:57 +0200

    HUB-72 add page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import PostsList, LikePost, TagPost, HotList
+from blog.views import PostsList, LikePost, TagPost, HotList, Terms
 
 urlpatterns = [
     path('', PostsList.as_view(), name='home' ),
     path("like/", LikePost.as_view(), name='like'),
     path("hot/", HotList.as_view(), name='hot'),
+    path("terms/", Terms.as_view(), name='terms'),
     path("tag/<slug:slug>/", TagPost.as_view(), name='tag_post'),
 ]
