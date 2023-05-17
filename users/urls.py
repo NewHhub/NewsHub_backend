188fdb320f50c2530692b6e50cc1e2e6858da028
Ruslan <sichkaryk39@gmail.com>
2023-05-17 16:34:48 +0300

    HUB-100 mail file

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from users.views import Register, Follow, Unfollow, profile_settings
+from users.views import Register, Follow, Unfollow, profile_settings, CustomPasswordResetView
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    path("password_reset/", CustomPasswordResetView.as_view(), name="password_reset"),
     path('register/', Register.as_view(), name='register'),
     path('follow/', Follow.as_view(), name='follow'),
     path('unfollow/', Unfollow.as_view(), name='unfollow'),
     path('profile-settings/', profile_settings, name='profile_settings'),
+    path('', include('django.contrib.auth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -6,6 +6,8 @@ from users.models import Followers
 from django.http import HttpResponseRedirect
 from .forms import ProfileSettingsForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordResetView
+from django.core.mail import EmailMessage
 
 
 # Create your views here.
@@ -85,3 +87,32 @@ def profile_settings(request):
     else:
         form = ProfileSettingsForm(instance=request.user)
     return render(request, 'profile/settings.html', {'form': form})
+
+
+class CustomPasswordResetView(PasswordResetView):
+    template_name = 'registration/password_reset_form.html' # ваш собственный шаблон формы для ввода email
+    html_email_template_name = 'registration/password_reset_email.html'
+    success_url = '/' # страница, на которую пользователь будет перенаправлен после отправки письма
+
+    def send_mail(self, subject_template_name, email_template_name, context, from_email, to_email, html_email_template_name=None):
+        """
+        Отправка HTML-письма.
+        """
+        subject = self.render_subject(subject_template_name, context)
+        # Генерация текстового содержимого письма
+        email_message = self.render_mail(email_template_name, context)
+
+        if html_email_template_name is not None:
+            # Генерация HTML-содержимого письма
+            html_email = self.render_mail(html_email_template_name, context)
+            # Установка типа содержимого письма как HTML
+            email_message.content_subtype = "html"
+            # Добавление HTML-содержимого в письмо
+            email_message.attach_alternative(html_email, "text/html")
+
+        email_message.send()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # добавьте дополнительные данные для контекста, если это необходимо
+        return context
